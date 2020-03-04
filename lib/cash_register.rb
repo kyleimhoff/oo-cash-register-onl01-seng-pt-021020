e24628bc4be89e96b7d0eb49c6997d3ba1c208c8
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-04 02:03:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -41,7 +41,7 @@ class CashRegister
     end
     
     def void_last_transaction
-      self.total = @total - (@transactions.pop * quantity)
+      self.total = @total - @transactions.pop
       
     end
   end 
